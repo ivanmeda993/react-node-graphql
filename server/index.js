54e957b467f7740597c3cf1e68ce93c19d6da933
IvanMilicevic <ivanmilicevicmeda@gmail.com>
2023-10-19 16:06:20 +0200

    db connection

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./lib/db");
+const cors = require("cors");
+const colors = require("colors");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
