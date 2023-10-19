de660163e1303fbac2acf7ed6ad9dbbe472cb2da
IvanMilicevic <ivanmilicevicmeda@gmail.com>
2023-10-19 15:12:35 +0200

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button.tsx";
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      test
+      <Button>test</Button>
+    </div>
+  );
+}
+
+export default App;
